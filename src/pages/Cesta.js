e23df78d521e062cb83a274e7ccd1e265a5cdc28
Hiago <gui.veroneze@gmail.com>
2022-08-11 16:11:45 -0300

    adding eslint

---
+++ src/pages/Cesta.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Cesta() {
+	return (
+		<View>
+		  <Text>
+        Cesta
+			</Text>
+			<Text>
+        Olá mundo
+			</Text>
+		</View>
+	)
+}
