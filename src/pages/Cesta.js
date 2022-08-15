53e3279b2e2f0479c1e3b19b2d8303c48d6a3294
Hiago <gui.veroneze@gmail.com>
2022-08-15 16:03:13 -0300

    estilizando a tela da cesta

--- src/pages/Cesta.js
+++ src/pages/Cesta.js
@@ -8,7 +8,15 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
 	return <>
 		<Image source={topo} style={estilos.topo}/>
-        <Text style={estilos.title} >Detalhes da cesta</Text>
+        <Text style={estilos.title}>Detalhes da cesta</Text>
+        <View style={estilos.container}>
+            <Text style={estilos.nomeCesta}>Cesta de Verduras</Text>
+            <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            <Text style={estilos.descricao}>Uma cesta com produtos selecionados
+             cuidadosamente da fazenda 
+             direto para sua cozinha</Text>
+            <Text style={estilos.preco}>R$40,00</Text>
+        </View>
 	</>
 }
 
@@ -27,5 +35,32 @@ const estilos = StyleSheet.create({
         fontWeight: "bold",
         padding: 16,
         lineHeight: 26
+    },
+    nomeCesta: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: "bold"
+    },
+    container: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
